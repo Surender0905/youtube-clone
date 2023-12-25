4bfc3b11be142242271b0c9b5e506ab2cec4c692
Surender Pratap Singh <rk31151@gmail.com>
2023-12-25 19:42:43 +0530

    connect client to api

--- api/index.js
+++ api/index.js
@@ -1,5 +1,8 @@
-require('dotenv').config();
-const express = require('express');
+import dotenv from 'dotenv';
+dotenv.config({
+  path: '../.env',
+});
+import express from 'express';
 const app = express();
 const port = process.env.PORT || 3000;
 
